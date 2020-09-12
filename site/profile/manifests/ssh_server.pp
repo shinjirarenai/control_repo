b068fbbf104d02699d4212e343ce586f1010b9cf
shinjirarenai <976031+shinjirarenai@users.noreply.github.com>
2020-09-12 22:50:06 +0900

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -11,3 +11,5 @@ class profile::ssh_server {
 		user   => 'root',
 		type   => 'ssh-rsa',
 		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDXNHNTxHYcCkXHBLj4kBapWu355dx8ivifkEk4OYwBhbmyr6NamCGk6HkZSaiALiSYucKbMLor1UHEabtG92LZCiLTRit+TCo4d90Fr0ZH5RxMJcz2MNNVC4SyGWA+adow9Mf1lrburo5Uc7Iuel7h0E7hktwTk7i0CfCZE+HmwaxHCpccI+2WsUfc64/3fVaJDOhOCA8bXlM39ImdLdFE57aungImN41utXg8h90gQ4FV6kX4USjERIxllVoStNFIhJRqDGmB8JlcKGYHIqL5l/8NMv4VO5rgIBlutxXmwj3dgsv9bpwlBXJCAoXOdhO/ypYB2OQvNIcq0N+nP9vh',
+		}
+}
