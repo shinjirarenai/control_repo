d15042f534f81df1783f2b4a1ab585ba3d11bf93
shinjirarenai <976031+shinjirarenai@users.noreply.github.com>
2020-09-12 22:56:19 +0900

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -5,4 +5,9 @@ class profile::agent_nodes {
   host {'web.puppet.vm':
     ensure => present,
     ip => '172.18.0.2',
+  }
+  host {'web.puppet.vm':
+    ensure => present,
+    ip => '172.18.0.3',
+  }
 }
