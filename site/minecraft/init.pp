3943181c7a93871a6e2439b8e3ce0ae8b8759b89
shinjirarenai <976031+shinjirarenai@users.noreply.github.com>
2020-09-13 11:35:41 +0900

    Update init.pp

--- site/minecraft/init.pp
+++ site/minecraft/init.pp
@@ -1,24 +1,24 @@
 class minecraft {
-  file {'/opt/minecraft':
-  ensure => directory,
+  file {‘/opt/minecraft’:
+    ensure => directory,
   }
-  file {'/opt/minecraft/minecraft_server.jar':
-  ensure => file,
-  source => '//https://s3.amazonaws.com/Minecraft.Download/versions/1.22.2/minecraft_server.1.12.2.jar',
+  file {‘/opt/minecraft/minecraft_server.1.12.2.jar’:
+    ensure => file,
+    source      => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   }
-  package {'java':
+  package {‘java’:
     ensure => present,
   }
-  file {'/opt/minecraft/eula.txt':
+  file {‘/opt/minecraft/eula.txt’:
     ensure => file,
-    content => 'eula=true'
+    content => ‘eula=true’
   }
-  file {'/etc/systemd/system/minecraft.service':
+  file {‘/etc/systemd/system/minecraft.service’:
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft_service',
+    source => ‘puppet:///modules/minecraft/minecraft.service’,
   }
-  service {'minecraft':
+  service { ‘minecraft’:
     ensure => running,
     enable => true,
-  }  
+  }
 }
